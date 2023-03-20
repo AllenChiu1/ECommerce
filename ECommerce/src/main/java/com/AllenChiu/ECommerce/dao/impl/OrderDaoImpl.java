9d1c3a4260d26d59775ed57c28f8256c97845929
AllenChiu <ghqzpm@gmail.com>
2023-03-20 23:54:27 +0800

    訂單功能 - 單元測試

--- ECommerce/src/main/java/com/AllenChiu/ECommerce/dao/impl/OrderDaoImpl.java
+++ ECommerce/src/main/java/com/AllenChiu/ECommerce/dao/impl/OrderDaoImpl.java
@@ -5,7 +5,6 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-import org.hibernate.validator.internal.engine.messageinterpolation.InterpolationTerm;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
